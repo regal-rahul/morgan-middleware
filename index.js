edd9d90016888f3d89ae48311a4394de359da741
regal-rahul <regalrahul777@gmail.com>
2022-06-07 18:31:51 +0530

    custom error class

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const AppError = require('./AppError');
+
+
 
 app.use(morgan('tiny'));
 
@@ -20,7 +23,9 @@ const verifyPassword = ((req, res, next) => {
         next();
     } else {
         // res.send('Sorry incorrect password!')
-        throw new Error('Password required');
+        // res.status(401)
+        // throw new Error('Password required');
+        throw new AppError('Password Required!', 401);
     }
 })
 
@@ -46,18 +51,24 @@ app.get('/dogs', (req, res) => {
 app.get('/secret', verifyPassword, (req, res) => {
     res.send('My secret is : Sometimes i eat!');
 })
-
+app.get('/admin', (req, res) => {
+    throw new AppError('You are not an Admin!', 403)
+})
 
 app.use((req, res) => {
     res.status(404).send('Not Found!');
 })
+// app.use((err, req, res, next) => {
+//     console.log('****************************')
+//     console.log('***********ERROR************')
+//     console.log('****************************')
+//     // res.status(500).send("OHH BOY! WE GOT AN ERROR!!!!!!!");
+//     // console.log(err)
+//     next(err)
+// })
 app.use((err, req, res, next) => {
-    console.log('****************************')
-    console.log('***********ERROR************')
-    console.log('****************************')
-    // res.status(500).send("OHH BOY! WE GOT AN ERROR!!!!!!!");
-    // console.log(err)
-    next(err)
+    const { status = 500, message = 'Something went Wrong!' } = err;
+    res.status(status).send(message);
 })
 
 app.listen(3000, () => {
